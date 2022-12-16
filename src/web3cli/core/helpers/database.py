6225fc3c20d618113bab808839283f8bd6cb8811
Guido W. Pettinari <guido.pettinari@idearia.it>
2022-12-16 05:39:14 +0100

    ✨ Use extended sqlite for db

--- src/web3cli/core/helpers/database.py
+++ src/web3cli/core/helpers/database.py
@@ -1,5 +1,5 @@
 from typing import List, Type
-from peewee import SqliteDatabase
+from playhouse.sqlite_ext import SqliteExtDatabase
 from web3cli.core.models.base_model import BaseModel, db
 from web3cli.core.models.address import Address
 from web3cli.core.models.chain import Chain, ChainRpc, Rpc
@@ -11,7 +11,7 @@ import os
 tables: List[Type[BaseModel]] = [Signer, Address, Chain, Rpc, ChainRpc, Tx]
 
 
-def init_db(db_path: str) -> SqliteDatabase:
+def init_db(db_path: str) -> SqliteExtDatabase:
     """Connect the global database object (db) to the given database file.
     If the database file does not exist, a new database file will be created
     at the given path, along with its parent folders."""
