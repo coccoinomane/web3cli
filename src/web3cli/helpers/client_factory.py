b45f93c8fbe3b53805e5f6e0939bd1b419b433ff
Guido W. Pettinari <guido.pettinari@idearia.it>
2023-01-15 23:10:33 +0100

    🧱 Helper function make_contract_client

--- src/web3cli/helpers/client_factory.py
+++ src/web3cli/helpers/client_factory.py
@@ -7,6 +7,9 @@ from web3client.base_client import BaseClient
 from web3cli.exceptions import Web3CliError
 from web3core.helpers.client_factory import make_base_client, make_base_wallet
 from web3core.helpers.client_factory import (
+    make_contract_client as make_contract_client_,
+)
+from web3core.helpers.client_factory import (
     make_contract_wallet as make_contract_wallet_,
 )
 from web3core.models.contract import Contract
@@ -34,20 +37,29 @@ def make_wallet(app: App, log: bool = False, **client_args: Any) -> BaseClient:
     )
 
 
-def make_contract_wallet(
+def make_contract_client(
     app: App,
     contract_name: str,
-    contract_type: str = None,
     log: bool = False,
     **client_args: Any,
 ) -> BaseClient:
     """Client suitable to interact with the given smart contract"""
-    # Fetch contract
-    contract = Contract.get_by_name_and_chain_or_raise(
-        contract_name.lower(), app.chain_name
+    return make_contract_client_(
+        contract_name=contract_name,
+        chain=app.chain,
+        node_uri=app.rpc,
+        logger=app.log.info if log else app.log.info if log else None,
+        **client_args,
     )
-    if contract.type != contract_type:
-        raise Web3CliError(f"Contract '{contract_name}' not of type '{contract_type}'")
+
+
+def make_contract_wallet(
+    app: App,
+    contract_name: str,
+    log: bool = False,
+    **client_args: Any,
+) -> BaseClient:
+    """Client suitable to interact with the given smart contract"""
     return make_contract_wallet_(
         contract_name=contract_name,
         chain=app.chain,
@@ -63,4 +75,4 @@ def make_erc20_wallet(
     app: App, token_name: str, log: bool = False, **client_args: Any
 ) -> BaseClient:
     """Client suitable to interact with the given ERC20 token"""
-    return make_contract_wallet(app, token_name, "erc20", log, **client_args)
+    return make_contract_wallet(app, token_name, log, **client_args)
