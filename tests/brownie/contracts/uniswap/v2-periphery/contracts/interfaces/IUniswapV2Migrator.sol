4e8d739cd0f05b9ce985dd2464b5f4b7700fdfb4
Guido W. Pettinari <coccoinomane@gmail.com>
2023-03-02 17:53:22 +0100

    🧪 Refactored uniswap contracts

--- tests/brownie/contracts/uniswap/v2-periphery/contracts/interfaces/IUniswapV2Migrator.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity >=0.5.0 <0.8.0;
-
-interface IUniswapV2Migrator {
-    function migrate(address token, uint amountTokenMin, uint amountETHMin, address to, uint deadline) external;
-}
