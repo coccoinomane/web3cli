4cc62e1f95be3e734d20ec9b31a43d30a0e30aa1
Guido W. Pettinari <coccoinomane@gmail.com>
2023-05-18 10:10:44 +0200

    ✨ Command 'w3 keyfile import'

--- src/web3cli/controllers/keyfile_controller.py
+++ src/web3cli/controllers/keyfile_controller.py
@@ -1,7 +1,8 @@
 import getpass
+import json
 
 from cement import ex
-from eth_keyfile import decode_keyfile_json, load_keyfile
+from eth_account import Account
 
 from web3cli.controllers.controller import Controller
 from web3cli.exceptions import Web3CliError
@@ -19,14 +20,31 @@ class KeyfileController(Controller):
     @ex(
         help="Print the private key of the given keyfile",
         arguments=[
-            (["keyfile"], {"help": "path to the JSON"}),
+            (["path"], {"help": "path to input keyfile (JSON)"}),
         ],
+        aliases=["decrypt"],
     )
     def decode(self) -> None:
-        keyfile_json = load_keyfile(self.app.pargs.keyfile)
+        with open(self.app.pargs.path) as f:
+            keyfile_json = json.load(f)
         password = getpass.getpass("Enter password to decrypt keyfile: ")
         try:
-            private_key = decode_keyfile_json(keyfile_json, password)
+            private_key = Account.decrypt(keyfile_json, password)
         except ValueError as e:
             raise Web3CliError(f"Could not decrypt keyfile: {e}")
         self.app.print(str(private_key.hex()[2:]))
+
+    @ex(
+        help="Create a new keyfile from a private key",
+        label="import",
+        arguments=[
+            (["path"], {"help": "path of output keyfile"}),
+        ],
+        aliases=["encrypt"],
+    )
+    def import_(self) -> None:
+        private_key = getpass.getpass("Enter the private key, without the leading 0x: ")
+        password = getpass.getpass("Enter a password to encrypt the keyfile: ")
+        keyfile_json = Account.encrypt(private_key=private_key, password=password)
+        json.dump(keyfile_json, open(self.app.pargs.path, "w"))
+        self.app.log.info(f"Keyfile saved to {self.app.pargs.path}")
