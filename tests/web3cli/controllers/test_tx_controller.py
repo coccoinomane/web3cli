f25b4945cb19c0c79e115888b13bf049d140bbdd
Guido W. Pettinari <coccoinomane@gmail.com>
2023-06-07 18:53:00 +0200

    🧪 Test w3 tx replay

--- tests/web3cli/controllers/test_tx_controller.py
+++ tests/web3cli/controllers/test_tx_controller.py
@@ -38,3 +38,50 @@ def test_tx_get_receipt(
     assert receipt.get("to") == bob.address
     assert type(receipt.get("gasUsed")) == int
     assert receipt.get("gasUsed") > 0
+
+
+@pytest.mark.local
+def test_tx_replay(
+    app: Web3CliTest, alice: ape.api.AccountAPI, bob: ape.api.AccountAPI
+) -> None:
+    bob_balance = bob.balance
+    original_tx = alice.transfer(bob, 10000)
+    assert bob.balance == bob_balance + 10000
+    app.set_args(
+        [
+            "tx",
+            "replay",
+            str(original_tx.txn_hash),
+            "--gas-multiplier",
+            "1",
+            "-s",
+            "alice",
+            "--force",
+        ]
+    ).run()
+    assert bob.balance == bob_balance + 20000
+
+
+@pytest.mark.local
+def test_tx_replay_contract_tx(
+    app: Web3CliTest,
+    alice: ape.api.AccountAPI,
+    bob: ape.api.AccountAPI,
+    TST: ape.contracts.ContractInstance,
+) -> None:
+    bob_balance = TST.balanceOf(bob.address)
+    original_tx = TST.transfer(bob, 10000, sender=alice)
+    assert TST.balanceOf(bob.address) == bob_balance + 10000
+    app.set_args(
+        [
+            "tx",
+            "replay",
+            str(original_tx.txn_hash),
+            "--gas-multiplier",
+            "1",
+            "-s",
+            "alice",
+            "--force",
+        ]
+    ).run()
+    assert TST.balanceOf(bob.address) == bob_balance + 20000
