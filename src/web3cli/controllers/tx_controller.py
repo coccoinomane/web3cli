6ad6390ea9c6077fb1ae44a32ac6815d4832c065
Guido W. Pettinari <coccoinomane@gmail.com>
2023-06-07 14:22:58 +0200

    ✨ Replay transaction command

--- src/web3cli/controllers/tx_controller.py
+++ src/web3cli/controllers/tx_controller.py
@@ -1,11 +1,15 @@
+import argparse
 import json
 
 from cement import ex
 from web3 import Web3
 
 from web3cli.controllers.controller import Controller
+from web3cli.exceptions import Web3CliError
 from web3cli.helpers import args
-from web3cli.helpers.client_factory import make_client
+from web3cli.helpers.client_factory import make_client, make_wallet
+from web3cli.helpers.render import render_web3py
+from web3core.helpers.misc import yes_or_exit
 
 
 class TxController(Controller):
@@ -43,3 +47,75 @@ class TxController(Controller):
         receipt = client.w3.eth.wait_for_transaction_receipt(self.app.pargs.hash)
         receipt_as_dict = json.loads(Web3.to_json(receipt))
         self.app.render(receipt_as_dict, indent=4, handler="json")
+
+    @ex(
+        help="replay the given transaction on the blockchain",
+        arguments=[
+            (["hash"], {"help": "hash of the transaction"}),
+            *args.chain_and_rpc(),
+            args.signer(),
+            args.priority_fee(
+                help="Optionally override priority fee, in gwei", default=None
+            ),
+            (
+                ["--preserve-type", "-p"],
+                {
+                    "help": "Whether to preserve the transaction type.  Disable on zkSync.",
+                    "action": argparse.BooleanOptionalAction,
+                    "default": True,
+                },
+            ),
+            args.tx_gas_limit(help="Optionally override gas limit"),
+            args.force(),
+        ],
+    )
+    def replay(self) -> None:
+        original_tx = make_client(self.app).get_tx(self.app.pargs.hash)
+        # Ask for confirmation
+        if not self.app.pargs.force:
+            self.app.log.info(f"Replaying the following transaction:")
+            render_web3py(self.app, original_tx)
+            yes_or_exit("\nContinue? ")
+        # Build replay transaction
+        signer = make_wallet(self.app)
+        tx = signer.build_base_tx()
+        # Make sure you call the same contract
+        tx["to"] = original_tx["to"]
+        # Make sure you call the same method with same args
+        if original_tx.get("input"):
+            tx["data"] = original_tx["input"]
+        # Make sure you pay the same amount
+        tx["value"] = original_tx["value"]
+        # Make sure you send the same type of transaction
+        if self.app.pargs.preserve_type and original_tx.get("type"):
+            tx["type"] = original_tx["type"]
+        # Make sure you use the same gas limit
+        tx["gas"] = original_tx["gas"]
+        # For post EIP-1559 transactions, use the same tip and max gas
+        if original_tx.get("maxFeePerGas"):
+            tx["maxFeePerGas"] = original_tx["maxFeePerGas"]
+        if original_tx.get("maxPriorityFeePerGas"):
+            tx["maxPriorityFeePerGas"] = original_tx["maxPriorityFeePerGas"]
+        # For pre EIP-1559 transactions, use the same gas price
+        if (
+            original_tx.get("gasPrice")
+            and not original_tx.get("maxFeePerGas")
+            and not original_tx.get("maxPriorityFeePerGas")
+        ):
+            tx["gasPrice"] = original_tx["gasPrice"]
+        # Optionally override original gas settings
+        if self.app.pargs.gas_limit:
+            tx["gas"] = self.app.pargs.gas_limit
+        if self.app.pargs.priority_fee:
+            tx["maxPriorityFeePerGas"] = Web3.to_wei(
+                self.app.pargs.priority_fee, "gwei"
+            )
+        # Send transaction
+        try:
+            tx_hash = signer.sign_and_send_tx(tx)
+        except TypeError as e:
+            raise Web3CliError(
+                f"Failed to send transaction: {e}. "
+                f"Try using --no-preserve-type to disable transaction type preservation."
+            )
+        self.app.print(tx_hash)
