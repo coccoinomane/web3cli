a7dac83a5ab6e4c9bdbf92ed82245f64128fef12
Guido W. Pettinari <coccoinomane@gmail.com>
2023-08-05 12:35:21 +0200

    🏷 Version bump 1.1.14

--- src/web3cli/helpers/version.py
+++ src/web3cli/helpers/version.py
@@ -3,7 +3,7 @@ from typing import Tuple
 from cement.utils.version import get_version as cement_get_version
 from cement.utils.version import get_version_banner
 
-VERSION = (1, 1, 13, "final", 0)
+VERSION = (1, 1, 14, "final", 0)
 
 
 def get_version_number(version: Tuple[int, int, int, str, int] = VERSION) -> str:
