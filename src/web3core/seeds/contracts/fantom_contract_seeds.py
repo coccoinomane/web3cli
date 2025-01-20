a2383e00cc512fb973e64394188f5362161a926b
Guido W. Pettinari <coccoinomane@gmail.com>
2025-01-20 17:12:46 +0100

    fix: wftm token in fantom had wrong type

--- src/web3core/seeds/contracts/fantom_contract_seeds.py
+++ src/web3core/seeds/contracts/fantom_contract_seeds.py
@@ -3,7 +3,7 @@ from web3core.models.types import ContractFields
 wftm: ContractFields = {
     "name": "wftm",
     "desc": "Wrapped Fantom",
-    "type": "wftm",
+    "type": "weth",
     "address": "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83",
     "chain": "fantom",
 }
